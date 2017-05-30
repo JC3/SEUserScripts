cec514aa99936c909f10df7e0b17b1c56054e7c9
JasonC <jason.cipriani@gmail.com>
2017-05-30 16:07:01 -0400

    SEDE Permalink

---
+++ SEDEPermalink.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         SEDE Parameterized Query Permalink
+// @namespace    https://stackexchange.com/users/305991/jason-c
+// @version      1.0
+// @description  Adds ability to get permalinks for parameterized queries.
+// @author       Jason C
+// @match        *://data.stackexchange.com/*/query/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('<button type="button"/>')
+        .addClass('btn-normal')
+        .text('Get Link')
+        .click(function () {
+            var link = [
+                window.location.protocol,
+                '//',
+                window.location.host,
+                window.location.pathname,
+                '?',
+                $('#runQueryForm').serialize(),
+                window.location.hash
+            ].join('');
+            window.prompt('Permalink:', link);
+        })
+        .insertAfter('#cancel-query');
+
+})();
