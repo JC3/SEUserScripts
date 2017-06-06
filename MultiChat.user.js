e8719698389edb227cfbd076f18572657b9783c2
JasonC <jason.cipriani@gmail.com>
2017-06-06 03:46:19 -0400

    progress
    more room styling

--- MultiChat.user.js
+++ MultiChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multi Chat
 // @namespace    http://stackexchange.com/users/305991/jason-c
-// @version      0.01-alpha
+// @version      0.02-alpha
 // @description  Multiple chat rooms in one window.
 // @author       Jason C
 // @match        *://chat.stackoverflow.com/multichat
@@ -152,48 +152,170 @@
 
     function assimilateRoom (dlg, doc) {
 
-        var roomName = doc.find('#roomname').text().trim();
-        var inputArea, roomMenu;
+        let roomName = doc.find('#roomname').text().trim();
         dlg.dialog('option', 'title', roomName);
-        doc.find('#reply-count').insertBefore(doc.find('#bubble'));
-        doc.find('#input-table tr:first td:first').remove();
-        doc.find('#input-table td.chat-input').removeAttr('rowspan').css({
-            'padding': '2px'
-        });
-        doc.find('#footer-logo').remove();
-        doc.find('#footer-legal').closest('tr').remove();
-        doc.find('#input').css({height:'auto'}).attr('rows', 2);
-        (inputArea = doc.find('#input-area')).css({
-            'padding': '0',
-            'height': `calc(10px + ${doc.find('#input').height()}px)`
-        });
-        doc.find('#bubble').css({
-            'height': 'auto',
+
+        // Some global styles for dynamically created elements.
+        let buttonFontSize = doc.find('#chat-buttons').css('font-size');
+        $('<style/>').attr('type', 'text/css')
+            .append('.popup { z-index: 1001; }\n')
+            .append('#room-tags { display: none; }\n') // Favorite button keeps recreating them in #info.
+            .append(`#chat-buttons .button { margin-left: 1ex; font-size: ${buttonFontSize}; }\n`)
+            .append('#chat-buttons .button:last-child { margin-right: 1ex; }\n')
+            .appendTo(doc.find('head'));
+
+        // 'attic' is where I'm putting stuff I want to remove but that is necessary for
+        // SE's scripts to function properly.
+        let attic = $('<div/>')
+            .css('display', 'none')
+            .attr('id', 'multichat-attic')
+            .appendTo(doc.find('body'));
+        doc.find('#about-room').appendTo(attic); // Needed when showing room menu.
+        doc.find('#roomname').appendTo(attic);   // Needed when showing room menu.
+        //doc.find('#room-tags').appendTo(attic);  // Needed by favorite button.
+
+        // Fix up the input area. Get rid of most of the stuff, make it compact. Drop
+        // the table and use divs instead, as well.
+        let inputLinkColor = doc.find('#footer-legal a:first').css('color');
+        let inputArea = doc.find('#input-area');
+        $('<div\>')
+            .attr('id', 'input-temp')
+            .appendTo(inputArea)
+            .append(inputArea.find('.input-hint-container'))
+            .append($('<div id="reply-spacer"\>').append(inputArea.find('#reply-count')))
+            .append($('<div class="chat-input"\>').append(inputArea.find('td.chat-input').children()))
+            .append($('<div id="chat-buttons-temp"\>').append(inputArea.find('#chat-buttons').children()));
+        inputArea.children(':not(#input-temp)').remove();
+        inputArea.append(inputArea.find('#input-temp').children());
+        inputArea.find('#input-temp').remove();
+        inputArea.find('#chat-buttons-temp').attr('id', 'chat-buttons');
+
+        // Style input area.
+        inputArea.children(':not(.input-hint-container)').css({
+            'display': 'inline-block',
             'width': 'auto',
-            'padding-left': '38px'
+            'height': 'auto'
         });
-        doc.find('#reply-count').css({
-            top: '7px',
-            left: '7px'
-        });
-        doc.find('#chat-buttons').css({
+        inputArea.find('.chat-input').css({
             'padding': '2px',
-            'padding-bottom': '5px',
-            'vertical-align': 'middle'
+            'padding-right': '0',
+            'flex-grow': '1'
         });
-        doc.find('#tabcomplete-container').css('top', '-33px');
+        inputArea.find('#input').attr('rows', '2').css({
+            'width': '100%',
+            'height': 'auto'
+        });
+        inputArea.find('#bubble').css({
+            'width': 'auto',
+            'height': `${inputArea.find('#input').height() + 6}px` // + 2 padding + 1 border
+        });
+        inputArea.find('#chat-buttons').css({
+            'white-space': 'nowrap',
+            'vertical-align': 'middle',
+            'flex-shrink': '0',
+            'padding': '2px',
+            'font-size': '0'
+        });
+        inputArea.css({
+            'display': 'flex',
+            'align-items': 'center',
+        });
+        inputArea[0].style.setProperty('height', `${inputArea.find('.chat-input').height() + 4}px`, 'important');
+        inputArea[0].style.setProperty('padding', '0', 'important');
+        inputArea.find('#reply-spacer').css({
+            'flex-shrink': '0',
+            'width': inputArea.height()
+        });
+        inputArea.find('#reply-count').css({
+            'position': 'static',
+            'top': 'auto',
+            'left': 'auto',
+            'padding': '0',
+            'width': '20px',
+            'height': '20px',
+            'margin': '0',
+            'margin-left': 'auto',
+            'margin-right': 'auto'
+        });
+        inputArea.find('#tabcomplete-container').css({
+            'top': '-29px'
+        });
+
+        // Clean up the info area. Discard what we don't need and move the rest
+        // (description + room controls) up to a full-width header bar.
+        doc.find('#info').attr('id', 'info-trash');
+        let info = $('<div/>')
+            .attr('id', 'info')
+            .append(doc.find('#roomdesc').css('flex-grow', '1'))
+            .append(doc.find('#toggle-favorite').css('flex-shrink', '0'))
+            .append(doc.find('#sound').css('flex-shrink', '0'))
+            .append(doc.find('#room-menu').css('flex-shrink', '0'))
+            .prependTo(doc.find('#sidebar-content'));
+        doc.find('#info-trash').remove();
+        info.prependTo(doc.find('body')).css({
+            'position': 'fixed',
+            'top': '0px',
+            'left': '0px',
+            'width': 'calc(100% - 8px)', // padding offset
+            'height': 'auto',
+            'padding': '4px',
+            'z-index': '1000',
+            'display': 'flex',
+            'align-items': 'center',
+            'background': doc.find('#input-area').css('background'),
+            'background-position-y': 'bottom',
+            'color': doc.find('#input-area').css('color')
+        }).children().css({
+            'display': 'inline-block'
+        });
+        info.find('a').css('color', inputLinkColor);
+        info.find('#sound').css({
+            'position': 'relative',
+            'top': '1px',
+            'left': '2px',
+            'margin-left': '1ex',
+            'margin-right': '1ex'
+        });
+
+        // Rest of the sidebar.
+        doc.find('#my-rooms').closest('.sidebar-widget').remove();
+        doc.find('#rejoin-favs').closest('.sidebar-widget').remove();
+        doc.find('#widgets .sidebar-widget:last').remove();
+
+        // Main chat area.
         doc.find('#chat').css('padding-bottom', `${inputArea.height()}px`);
         doc.find('#container').css({
             'padding-left': '0',
             'padding-right': '0',
             'padding-bottom': '2px'
         });
-        doc.find('#allrooms, #searchbox, #roomname, #room-tags').remove();
-        doc.find('#my-rooms').closest('.sidebar-widget').remove();
-        doc.find('#rejoin-favs').closest('.sidebar-widget').remove();
-        doc.find('#widgets .sidebar-widget:last').remove();
-        doc.find('.feed-icon').closest('a').remove();
-        (roomMenu = doc.find('#room-menu')).closest('#sidebar-menu').empty().append(roomMenu);
+
+        // Use the top margin of the chat area and sidebar to make space for the new
+        // room info header.
+        doc.find('#container, #sidebar').css({
+            'margin-top': `${info.height() + 8}px`
+        });
+
+        // The sidebar now contains only the room inhabitant avatars. Make it a bit
+        // narrower and resize the chat area to match.
+        doc.find('#sidebar').css({
+            'padding-right': '0',
+            'width': '120px'
+        });
+        doc.find('#main').css({
+            'width': `calc(100% - ${doc.find('#sidebar').width()}px)`
+        });
+
+        // Just for now until I figure out what to do with star wall.
+        doc.find('#starred-posts').closest('.sidebar-widget').remove();
+
+        // Disable image uploader. It can't work in an iframe because of line 4191 (in the
+        // prettified version) of master-chat.js, where initFileUpload() uses top.document
+        // as the basis for all the new elements, so the uploader stuff ends up being created
+        // in our actual page instead of in the frame (and no, moving the uploader elements
+        // back into the iframe doesn't work, it's already busted at that point).
+        doc.find('#upload-file').remove();
+
     }
 
 })();
